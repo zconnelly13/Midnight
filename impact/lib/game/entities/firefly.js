34bb86e0de8e11ef390a6de83be649b7b05fe025
Zac Connelly <zconnelly13@gmail.com>
2014-06-05 20:53:21 +0000

    firefly draw method (wrong)

--- impact/lib/game/entities/firefly.js
+++ impact/lib/game/entities/firefly.js
@@ -1,51 +1,32 @@
 ig.module(
-    'game.entities.midnight'
+    'game.entities.firefly'
 )
 .requires(
     'impact.entity'
 )
 .defines(function() {
     
-    EntityMidnight = ig.Entity.extend({
+    EntityFirefly = ig.Entity.extend({
         _wmScalable: true,
-        size: {x: 89, y:122},
+        size: {x: 10, y:10},
         maxVel: { x: 500, y: 500 },
         collides: ig.Entity.COLLIDES.ACTIVE,
         name: "Midnight",
 
         speed: 200,
-        jumpStrength: 128,
-
-        animSheet: new ig.AnimationSheet('media/images/characters/midnight_running_photoshop.png', 89, 128),
+        gravityFactor: 0,
 
         init: function(x, y, settings) {
             this.parent(x,y,settings);
-            var animOrder = [];
-            for(var i=0;i<60;i++) {
-                animOrder.push(i);
-            }
-            this.addAnim('default',0.035/2,animOrder);
         },
-
         
-        handleMovement: function() {
-            if (ig.input.state('left')) {
-                this.currentAnim.flip.x = true;
-                this.vel.x = -this.speed;
-            } else if (ig.input.state('right')) {
-                this.currentAnim.flip.x = false;
-                this.vel.x = this.speed;
-            } else {
-                this.vel.x = 0;
-            }
-            if (ig.input.pressed('up') && this.standing) {
-                this.vel.y = -this.jumpStrength;
-            }
-        },
-
         update: function() {
             this.parent();
-            this.handleMovement();
+        },
+
+        draw: function() {
+            ig.system.context.fillStyle = "yellow";
+            ig.system.context.fillRect(this.pos.x + ig.game.screen.x,this.pos.y + ig.game.screen.y,10,10);
         },
 
     });
