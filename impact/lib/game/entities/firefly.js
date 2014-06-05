c377ff5d9aeefa07cbcf8ad416f1aebd1fc489b0
Zac Connelly <zconnelly13@gmail.com>
2014-06-05 20:44:15 +0000

    firefly initial commit

---
+++ impact/lib/game/entities/firefly.js
@@ -0,0 +1,53 @@
+ig.module(
+    'game.entities.midnight'
+)
+.requires(
+    'impact.entity'
+)
+.defines(function() {
+    
+    EntityMidnight = ig.Entity.extend({
+        _wmScalable: true,
+        size: {x: 89, y:122},
+        maxVel: { x: 500, y: 500 },
+        collides: ig.Entity.COLLIDES.ACTIVE,
+        name: "Midnight",
+
+        speed: 200,
+        jumpStrength: 128,
+
+        animSheet: new ig.AnimationSheet('media/images/characters/midnight_running_photoshop.png', 89, 128),
+
+        init: function(x, y, settings) {
+            this.parent(x,y,settings);
+            var animOrder = [];
+            for(var i=0;i<60;i++) {
+                animOrder.push(i);
+            }
+            this.addAnim('default',0.035/2,animOrder);
+        },
+
+        
+        handleMovement: function() {
+            if (ig.input.state('left')) {
+                this.currentAnim.flip.x = true;
+                this.vel.x = -this.speed;
+            } else if (ig.input.state('right')) {
+                this.currentAnim.flip.x = false;
+                this.vel.x = this.speed;
+            } else {
+                this.vel.x = 0;
+            }
+            if (ig.input.pressed('up') && this.standing) {
+                this.vel.y = -this.jumpStrength;
+            }
+        },
+
+        update: function() {
+            this.parent();
+            this.handleMovement();
+        },
+
+    });
+
+});
